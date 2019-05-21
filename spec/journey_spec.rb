70e365c3bd6a45e22d92d2dc875f79b04b482f71
Helen Hayward <helen.m.hayward@hotmail.co.uk>
2019-05-21 22:23:36 +0100

    Refactored journey_spec

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -4,43 +4,38 @@ require 'oystercard'
 describe Journey do
 let (:station_dbl) {double("Station")}
 
-  it 'Journey class' do
-    expect(subject).to be_an_instance_of(Journey)
-  end
-
   it 'starting a journey' do
     expect(subject.start(station_dbl)).to eq(station_dbl)
   end
-
-  it 'finishing a journey' do
-    expect(subject.finish(station_dbl)).to eq(station_dbl)
-  end
-
-  it 'Is the journey complete' do
-    subject.start(station_dbl)
-    subject.finish(station_dbl)
-    expect(subject.completed).to eq(true)
-  end
-
   it '#touch_in(entry_station)' do
     subject.start(station_dbl)
     expect(subject.entry_station).to eq(station_dbl)
   end
-
-  it '#fare - Fare of £1' do
-    subject.start(station_dbl)
-    subject.finish(station_dbl)
-    expect(subject.fare).to eq(Journey::MINIMUM_FARE)
+  it 'finishing a journey' do
+    expect(subject.finish(station_dbl)).to eq(station_dbl)
   end
 
-  it '#fare - No entry station' do
-    subject.finish(station_dbl)
-    expect(subject.fare).to eq(Journey::PENALTY)
+  context 'Encurring penalty charge' do
+    it '#fare - No exit station' do
+      subject.start(station_dbl)
+      expect(subject.fare).to eq(Journey::PENALTY)
+    end
+    it '#fare - No entry station' do
+      subject.finish(station_dbl)
+      expect(subject.fare).to eq(Journey::PENALTY)
+    end
   end
 
-  it '#fare - No exit station' do
-    subject.start(station_dbl)
-    expect(subject.fare).to eq(Journey::PENALTY)
+  context 'Completed journeys' do
+    before { subject.start(station_dbl)
+      subject.finish(station_dbl)
+    }
+    it 'Is the journey complete' do
+      expect(subject.completed).to eq(true)
+    end
+    it '#fare - Fare of £1' do
+      expect(subject.fare).to eq(Journey::MINIMUM_FARE)
+    end
   end
 
 end
