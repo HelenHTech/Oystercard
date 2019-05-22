6369a19f7b8c7de2a1dab7d8c669aeaa4045c575
edwardreeve <edreeve+git@gmail.com>
2019-05-22 09:31:20 +0100

    few formatting changes

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,36 +1,45 @@
+# A card to store funds and pay for travel
 class Oystercard
-  MAXIMUM_BALANCE = 9000
-  MINIMUM_BALANCE = Journey::MINIMUM_FARE
-  MAX_ERROR = "Maximum limit (of £#{MAXIMUM_BALANCE/100}) reached"
-  MIN_ERROR = "Minimum fare of £#{MINIMUM_BALANCE/100} is required to touch in"
-  attr_reader :balance, :entry_station, :list_of_journeys, :exit_station
-  def initialize(balance=0)
+  attr_reader :balance, :entry_station, :journey_list, :exit_station
+  MAX_BALANCE = 9000
+  MIN_BALANCE = Journey::MINIMUM_FARE
+  MAX_ERROR = "Maximum limit (of £#{MAX_BALANCE / 100}) reached".freeze
+  MIN_ERROR = "Minimum fare of £#{MIN_BALANCE / 100} is required to touch in".freeze
+  def initialize(balance = 0)
     @balance = balance
-    @list_of_journeys = []
-    @current_journey
+    @journey_list = []
+    @current_journey = nil
   end
 
   def top_up(amount)
-    raise "Maximum limit (of £#{MAXIMUM_BALANCE/100}) reached" if @balance + amount > MAXIMUM_BALANCE
+    raise MAX_ERROR if @balance + amount > MAX_BALANCE
+
     @balance += amount
   end
+
   def in_journey?
     !@current_journey.completed
   end
+
   def touch_in(station)
-    raise "Minimum fare of £1 is required to touch in" if !minimum_balance_met?
+    raise MIN_ERROR unless min_balance_met?
+
     @current_journey = Journey.new
-    @current_journey.start(station)  
+    @current_journey.start(station)
   end
+
   def touch_out(exit_station)
-    deduct(MINIMUM_BALANCE)
+    deduct(MIN_BALANCE)
     @current_journey.finish(exit_station)
-    @list_of_journeys.push(@current_journey.journey_hash)
+    @journey_list.push(@current_journey.journey_hash)
   end
-  def minimum_balance_met?
-    @balance >= MINIMUM_BALANCE
+
+  def min_balance_met?
+    @balance >= MIN_BALANCE
   end
+
   private
+
   def deduct(amount)
     @balance -= amount
   end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,24 +2,25 @@ require 'oystercard'
 require 'journey'
 
 describe Oystercard do
-  let (:station_dbl) {double("Station")}
-  let(:minimum) {Oystercard::MINIMUM_BALANCE}
-  let(:maximum) {Oystercard::MAXIMUM_BALANCE}
+  let(:station) { double('Station') }
+  let(:min) { Oystercard::MIN_BALANCE }
+  let(:max) { Oystercard::MAX_BALANCE }
 
   it 'Top up card with minimum balance' do
-    expect{subject.top_up(minimum)}.to change{subject.balance}.by(minimum)
+    expect { subject.top_up(min) }.to change{ subject.balance }.by(min)
   end
 
   it 'Error if maximum balance is exceeded' do
-    expect{subject.top_up(maximum+1)}.to raise_error(Oystercard::MAX_ERROR)
+    expect { subject.top_up(max + 1) }.to raise_error(Oystercard::MAX_ERROR)
   end
 
   context 'Top-up with minimum fare of £1' do
-    before {subject.top_up(minimum)
-      subject.touch_in(station_dbl)
-    }
+    before do
+      subject.top_up(min)
+      subject.touch_in(station)
+    end
     it 'Deducted fare from card balance' do
-      expect{subject.touch_out(station_dbl)}.to change{subject.balance}.by(-minimum)
+      expect { subject.touch_out(station) }.to change { subject.balance }.by(-min)
     end
     it 'Shows as on-gonig journey' do
       expect(subject.in_journey?).to eq(true)
@@ -28,21 +29,21 @@ describe Oystercard do
 
   context 'New card with £0 balance' do
     it 'Raise error when card is touched in' do
-      expect{subject.touch_in(station_dbl)}.to raise_error(Oystercard::MIN_ERROR)
+      expect { subject.touch_in(station) }.to raise_error(Oystercard::MIN_ERROR)
     end
   end
 
   context 'Given a completed journey' do
-    before(:each) {
-      subject.top_up(minimum)
-      subject.touch_in(station_dbl)
-      subject.touch_out(station_dbl)
-    }
+    before(:each) do
+      subject.top_up(min)
+      subject.touch_in(station)
+      subject.touch_out(station)
+    end
     it 'Joruney is marked Completed - Card shows not in journey' do
       expect(subject.in_journey?).to eq(false)
     end
-    it 'Completd journey stored on card' do
-      expect(subject.list_of_journeys).to eq([{:entry => station_dbl, :exit => station_dbl}])
+    it 'Completed journey stored on card' do
+      expect(subject.journey_list).to eq([{ entry: station, exit: station }])
     end
   end
 end
