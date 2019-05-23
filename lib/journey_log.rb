15aa548fce5be67a4652bffa01f35beadd26338b
Helen Hayward <helen.m.hayward@hotmail.co.uk>
2019-05-23 18:15:34 +0100

    Journeylog class updated

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,25 +1,26 @@
 require 'journey'
-require 'oystercard'
 
 class Journeylog
-  attr_reader :journey_class, :journeys
-  def initialize(journey_class = Journey.new)
-    @journey_class = journey_class
-    @journeys = []
-    @current_journey  #  return a list of all previous journeys without exposing the internal array to external modification
+  attr_reader :journeys
+  def initialize(journey = Journey.new)
+    @journeys = [journey]
   end
 
-  def start(station)
-    @journey_class.entry_station = station
-    @journeys.push(journey_class.entry_station)
+  def start_journey(station)
+    @journeys[0].start(station)
   end
 
-  # def finish(station)
-  #   station #  Added exit station to current journey
-  # end
+  def finish_journey(station)
+    @journeys[0].finish(station)
+  end
 
+  private
   def current_journey
-    @current_journey ||= @journey_class
+    if @journeys[0].complete?
+      @journeys.push(journey)
+    else
+      return @journeys
+    end
   end
 
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,26 +1,26 @@
 require 'journey_log'
 require 'journey'
-require 'oystercard'
 
 describe Journeylog do
-let(:station) { double(:station) }
-let(:journey) { double(:journey) }
-let(:journey_class){ double(:journey_class)}
+  let(:station) { double(:station) }
+  let(:journeylog) { Journeylog.new }
 
 context 'Logging a journey' do
-  it 'Starting a journey' do
-    subject.start(station)
-    expect(subject.current_journey.entry_station).to eq(station)
+  it 'Starting a journey - Entry station' do
+    journeylog.start_journey(station)
+    start_journey = journeylog.journeys[0]
+    expect(start_journey.entry_station).to eq(station)
   end
-  # it 'Finish station' do
-  #   subject.finish(station)
-  #   expect(subject.exit_station).to eq(station)
-  # end
+  it 'Finishing a journey - Exit station' do
+    journeylog.finish_journey(station)
+    finish_journey = journeylog.journeys[0]
+    expect(finish_journey.exit_station).to eq(station)
+  end
 end
 
-it 'Records journey' do
-  subject.start(station)
-  expect(subject.journeys).to include(station)
-end
+  # it 'Current_journey method to return only incomplete journey' do
+  #   journeylog.start(station)
+  #   expect(journeylog.current_journey).to eq(journeylog.journeys[0])
+  # end
 
 end
